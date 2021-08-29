08e7129dcf1d960746e906356dfd270e533ef1f3
StuartGlass <stuart.glass@iplato.com>
2021-08-29 22:49:53 +0100

    added PWA and hallway lighting

--- public/main.js
+++ public/main.js
@@ -1,7 +1,8 @@
 const rooms = {
   lounge: 14,
   kitchen: 13,
-  bedroom: 16
+  bedroom: 16,
+  hallway: 12
 }
 
 const values = {
@@ -14,4 +15,21 @@ function lights(area, value) {
   fetch(`/lights/${rooms[area]}/${values[value]}`)
     .then((response) => response.json())
     .then((data) => console.log(data))
+}
+
+// PWA
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./service-worker.js").then(
+      (registration) => {
+        console.log(
+          `ServiceWorker registration successful with scope: ${registration.scope}`
+        )
+      },
+      (error) => {
+        console.log(`ServiceWorker registration failed: ${error}`)
+      }
+    )
+  })
 }
